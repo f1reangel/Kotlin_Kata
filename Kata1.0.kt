7eb501ad64f3919d0b12e2cc2681b586a7d53bd3
SoulfulByte <54482411+f1reangel@users.noreply.github.com>
2023-09-24 00:03:21 +0300

    Create Kata1.0.kt
    You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
    
    Write a program that returns the girl's age (0-9) as an integer.
    
    Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.

---
+++ Kata1.0.kt
@@ -0,0 +1,14 @@
+package basic
+
+class ParseIntCharProblem {
+    
+   fun getAge(yearsOld: String): Int {
+        
+     var res = yearsOld.first().digitToInt()
+     return res
+       
+      
+       
+   }
+    
+}
