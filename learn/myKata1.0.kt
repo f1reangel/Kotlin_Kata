c3c900ba58626a4e22d9c18a77b85238f7747f71
SoulfulByte <54482411+f1reangel@users.noreply.github.com>
2023-09-27 00:42:58 +0300

    Create myKata1.0.kt

---
+++ learn/myKata1.0.kt
@@ -0,0 +1,16 @@
+fun fil(str:List<String>):List<String>{
+   val duplicates = str.groupBy { it }
+        .filter { it.value.size > 1 }
+        .map { it.key }
+    return(duplicates)
+}
+fun fill(str:List<String>):List<String> {
+    var notDuplicates = str.filter { element -> str.count { it == element } == 1 }
+    return notDuplicates
+}
+fun main() {
+    var str = listOf("Wow", "Wow", "GG", "W", "W")
+   	println(fil(str))
+    println(fill(str))
+  
+}
