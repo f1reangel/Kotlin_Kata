3786e5f58662b0cfbb54f9d8edb5b83fb98d5fea
SoulfulByte <54482411+f1reangel@users.noreply.github.com>
2023-10-03 21:23:09 +0300

    Create Kata1.3.kt
    You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
    
    You can assume all values in the array are numbers.

---
+++ Kata1.3.kt
@@ -0,0 +1,9 @@
+fun smallEnough(a : IntArray, limit : Int) : Boolean {
+  return a.none { it > limit }
+}
+
+//Also takes a higher-order function as param to predicate the condition and return 
+//Boolean as true if all of the items in List, Set or Map confirms that condition, else return false.
+
+//Приймає функцію вищого порядку як параметр для визначення умови та повертає як істину , 
+//Booleanякщо всі елементи в або підтверджує цю умову, інакше повертає false .
